1e2df2464f9fc703a1fabc90892d87fa946b158a
(jyh) <(jikkk9@naver.com)>
2023-08-14 11:14:00 +0900

    add ( section05 반응형 완료  ) , 나중에 디테일은 다시해야함

--- tim/src/components/Section01.js
+++ tim/src/components/Section01.js
@@ -7,7 +7,6 @@ import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-
 import "../css/section01.css";
 
 function Section01() {
--- tim/src/components/Section05.js
+++ tim/src/components/Section05.js
@@ -4,9 +4,14 @@ import List from '../data/section05Data';
 function Section05({ data }) {
   return (
     < >
-      <div>
-        <img className='waveImage' src={`${process.env.PUBLIC_URL}/img/icon/wave.png`}></img>
+      <div className="wave-container">
+        <div className="wave">
+          <img src={`${process.env.PUBLIC_URL}/img/icon/wave.png`} />
+          <img className='wave2' src={`${process.env.PUBLIC_URL}/img/icon/wave.png`} />
+
+        </div>
       </div>
+
       <h2 className='Section05_H2'><b>편의시설</b></h2>
       <div className='Section05 inner'>
         {List.map(item => (
