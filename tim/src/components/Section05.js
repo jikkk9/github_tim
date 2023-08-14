2830332dcb841f8bc89249776ac8306ddf33f516
(jyh) <(jikkk9@naver.com)>
2023-08-14 10:22:53 +0900

    add (section05) 완료 반응형 작업 남음

--- tim/src/components/Section05.js
+++ tim/src/components/Section05.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import List from '../data/section05Data';
 
-function Section05() {
+function Section05({ data }) {
   return (
-    <div>Section05</div>
+    < >
+      <div>
+        <img className='waveImage' src={`${process.env.PUBLIC_URL}/img/icon/wave.png`}></img>
+      </div>
+      <h2 className='Section05_H2'><b>편의시설</b></h2>
+      <div className='Section05 inner'>
+        {List.map(item => (
+          <div key={item._id}>
+            <img src={`${process.env.PUBLIC_URL}/img/icon/${item.img}.png`} className="staticImage"></img>
+
+            <img src={`${process.env.PUBLIC_URL}/img/icon/${item.img}.gif`} className="gifImage"></img>
+            <img src=''></img>
+            <h3>{item.title}</h3>
+
+
+
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
--- tim/src/data/section05Data.js
+++ tim/src/data/section05Data.js
@@ -1,70 +1,70 @@
-let list = [
+let section05Data = [
   {
     _id: 1,
-    img: 'icon_24hours.gif',
+    img: 'icon_24hours',
     title: '24시간 시설 이용',
     subTitle: '서브 타이틀 1',
     test1: "추가 텍스트1",
-    category: 'books',
+    category: 'section05',
 
   },
   {
     _id: 2,
-    img: 'icon_deliveryservice.gif',
+    img: 'icon_deliveryservice',
     title: '택배 서비스',
     subTitle: '서브 타이틀 1',
     test1: "추가 텍스트1",
-    category: 'books',
+    category: 'section05',
 
   },
   {
     _id: 3,
-    img: 'icon_fingerprint.gif',
+    img: 'icon_fingerprint',
     title: '지문 인식 시스템',
     subTitle: '서브 타이틀 1',
     test1: "추가 텍스트1",
-    category: 'books',
+    category: 'section05',
 
   },
   {
     _id: 4,
-    img: 'icon_library.gif',
+    img: 'icon_library',
     title: '도서관',
     subTitle: '서브 타이틀 1',
     test1: "추가 텍스트1",
-    category: 'books',
+    category: 'section05',
 
   },
   {
     _id: 5,
-    img: 'icon_mailboxservices.gif',
+    img: 'icon_mailboxservices',
     title: '우편함 서비스',
     subTitle: '서브 타이틀 1',
     test1: "추가 텍스트1",
-    category: 'books',
+    category: 'section05',
 
   },
   {
     _id: 6,
-    img: 'icon_oaservices.gif',
+    img: 'icon_oaservices',
     title: 'OA 서비스',
     subTitle: '서브 타이틀 1',
     test1: "추가 텍스트1",
-    category: 'books',
+    category: 'section05',
 
   },
   {
     _id: 7,
-    img: 'icon_restarea.gif',
+    img: 'icon_restarea',
     title: '휴식공간',
     subTitle: '서브 타이틀 1',
     test1: "추가 텍스트1",
-    category: 'books',
+    category: 'section05',
 
   },
   {
     _id: 8,
-    img: 'icon_studio.gif',
+    img: 'icon_studio',
     title: '스튜디오',
     subTitle: '서브 타이틀 1',
     test1: "추가 텍스트1",
@@ -72,4 +72,4 @@ let list = [
 
   },
 ];
-export default list;
+export default section05Data;
