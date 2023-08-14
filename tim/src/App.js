d7db7577748b22448e4fc08dfde7d45ee28cf6f8
leehj <leehj4590@gmail.com>
2023-08-14 10:56:12 +0900

    add (section01.css 추가, section01 작업 완료, swiper 플러그인 추가)

--- tim/src/App.js
+++ tim/src/App.js
@@ -1,21 +1,28 @@
-import './css/App.css';
-import './css/reset.css';
-import './css/section04.css';
-import './css/section04_List.css';
-import './css/section05.css';
+import "./css/App.css";
+import "./css/reset.css";
+import "./css/section04.css";
+import "./css/section04_List.css";
+import "./css/section05.css";
 
-import { Routes, Route, Link, useLocation, useNavigate, Outlet } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+  Link,
+  useLocation,
+  useNavigate,
+  Outlet,
+} from "react-router-dom";
 
-import Main from './components/Main';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import retiringroom from './data/retiringroom';
-import section05Data from './data/section05Data';
-import { useState } from 'react';
-import Section04 from './components/Section04';
-import Section02 from './components/Section02';
-import Section05 from './components/Section05';
-
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import retiringroom from "./data/retiringroom";
+import section05Data from "./data/section05Data";
+import { useState } from "react";
+import Section04 from "./components/Section04";
+import Section02 from "./components/Section02";
+import Section05 from "./components/Section05";
+import Section01 from "./components/Section01";
 
 function CombinedSections({ data, data05 }) {
   return (
@@ -32,16 +39,17 @@ function App() {
   return (
     <div className="App">
       <Header title="Header" />
+      <Section01></Section01>
       <Section02></Section02>
-
       <Routes>
-        <Route path='/' element={<CombinedSections data05={data05} data={data} />}></Route>
-
+        <Route
+          path="/"
+          element={<CombinedSections data05={data05} data={data} />}
+        ></Route>
       </Routes>
 
-
       <Footer />
-    </div >
+    </div>
   );
 }
 
--- tim/src/components/Section01.js
+++ tim/src/components/Section01.js
@@ -1,11 +1,47 @@
-import React from "react";
+import { React, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const Section01 = () => {
+import { Pagination } from "swiper/modules";
+
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "../css/section01.css";
+
+function Section01() {
   return (
-    <div>
-      <h1>Section</h1>
+    <div className="Section01">
+      <Swiper
+        slidesPerView={"auto"}
+        loop={true}
+        spaceBetween={20}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <h2>
+          모두를 위한, <br />
+          작업공간
+        </h2>
+        <SwiperSlide className="bg">
+          <img src="/img/section01/slideImg01.jpg" />
+        </SwiperSlide>
+        <SwiperSlide className="bg">
+          <img src="/img/section01/slideImg02.jpg" />
+        </SwiperSlide>
+        <SwiperSlide className="bg">
+          <img src="/img/section01/slideImg03.jpg" />
+        </SwiperSlide>
+        <SwiperSlide className="bg">
+          <img src="/img/section01/slideImg04.jpg" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
-};
+}
 
 export default Section01;
