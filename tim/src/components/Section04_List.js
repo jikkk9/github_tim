62c2e97a6030feb35c383e9f4d50534d17085b96
Lhjin90 <leehj4590@gmail.com>
2023-08-29 11:30:09 +0900

    add (css 수정)

--- tim/src/components/Section04_List.js
+++ tim/src/components/Section04_List.js
@@ -1,42 +1,39 @@
-import React, { useEffect, useState } from 'react' // useEffect 추가
-
-
+import React, { useEffect, useState } from "react"; // useEffect 추가
 
 function Section04_List({ data, selectedCategory }) {
-
   // 이미지에 대한 조건부 스타일
-  const imageStyle = selectedCategory === 'books' ? { objectFit: 'contain' } : {};
-
+  const imageStyle =
+    selectedCategory === "books" ? { objectFit: "contain" } : {};
 
   const [dataList, setDataList] = useState([data]);
   useEffect(() => {
-    setDataList(data);  // data prop이 변경될 때마다 dataList 상태 업데이트
+    setDataList(data); // data prop이 변경될 때마다 dataList 상태 업데이트
   }, [data]);
 
   return (
     <>
-      <section className='Section04_List'>
-        <ul className='Section04_List_UL'>
+      <section className="Section04_List">
+        <ul className="Section04_List_UL">
           {dataList.map((item) => (
-            <li className='list' key={item._id}>
+            <li className="list" key={item._id}>
               {/* <h3>ID : {item.category}</h3> */}
               <b>
-                <img style={imageStyle} src={`${process.env.PUBLIC_URL}/img/section04/${item.img} `}></img>
+                <img
+                  style={imageStyle}
+                  src={`${process.env.PUBLIC_URL}/img/section04/${item.img} `}
+                ></img>
               </b>
 
               <h2>{item.title}</h2>
-              <p>{item.subTitle}</p>
-              <p>{item.price}</p>
+              <p className="listSubTitle">{item.subTitle}</p>
+              <p className="listPrice">{item.price}</p>
               <p>{item.test1}</p>
-
-
-
             </li>
           ))}
         </ul>
       </section>
     </>
-  )
+  );
 }
 
-export default Section04_List
+export default Section04_List;
--- tim/src/components/Section05.js
+++ tim/src/components/Section05.js
@@ -1,34 +1,39 @@
-import React from 'react'
-import List from '../data/section05Data';
+import React from "react";
+import List from "../data/section05Data";
 
 function Section05({ data }) {
   return (
-    < >
-      <div className="wave-container">
+    <>
+      {/*   <div className="wave-container">
         <div className="wave">
           <img src={`${process.env.PUBLIC_URL}/img/icon/wave.png`} />
           <img className='wave2' src={`${process.env.PUBLIC_URL}/img/icon/wave.png`} />
 
         </div>
-      </div>
+      </div> */}
 
-      <h2 className='Section05_H2'><b>편의시설</b></h2>
-      <div className='Section05 inner'>
-        {List.map(item => (
+      <h2 className="Section05_H2">
+        <b>편의시설</b>
+      </h2>
+      <div className="Section05 inner">
+        {List.map((item) => (
           <div key={item._id}>
-            <img src={`${process.env.PUBLIC_URL}/img/icon/${item.img}.png`} className="staticImage"></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/img/icon/${item.img}.png`}
+              className="staticImage"
+            ></img>
 
-            <img src={`${process.env.PUBLIC_URL}/img/icon/${item.img}.gif`} className="gifImage"></img>
-            <img src=''></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/img/icon/${item.img}.gif`}
+              className="gifImage"
+            ></img>
+            <img src=""></img>
             <h3>{item.title}</h3>
-
-
-
           </div>
         ))}
       </div>
     </>
-  )
+  );
 }
 
-export default Section05
+export default Section05;
